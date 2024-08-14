da85bb7555c47d37fa717f13d8f3a14439cb64e5
quyang <1633433173@qq.com>
2024-08-14 14:11:05 +0800

    完成抽取MQ工具类

--- hmall/hm-common/src/main/java/com/hmall/common/config/MqConsumeErrorAutoConfiguration.java
+++ hmall/hm-common/src/main/java/com/hmall/common/config/MqConsumeErrorAutoConfiguration.java
@@ -1,20 +1,41 @@
 package com.hmall.common.config;
 
-import org.springframework.amqp.rabbit.annotation.Exchange;
-import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.retry.MessageRecoverer;
+import org.springframework.amqp.rabbit.retry.RepublishMessageRecoverer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ConditionalOnProperty(name = "spring.rabbitmq.listener.simple.retry.enabled", havingValue = "true")
 public class MqConsumeErrorAutoConfiguration {
+
+    @Value("${spring.application.name}")
+    private String serviceName;
 
     @Bean
     public Queue errorQueue() {
-
+        return new Queue(serviceName + "error.queue", true);
     }
 
     @Bean
-    public Exchange errorExchange() {
+    public DirectExchange errorExchange() {
+        return new DirectExchange("error.direct");
+    }
 
+    @Bean
+    public Binding errorQueueBinding(Queue errorQueue, DirectExchange errorExchange) {
+        return BindingBuilder.bind(errorQueue).to(errorExchange).with(serviceName);
+    }
+
+    @Bean
+    public MessageRecoverer messageRecoverer(RabbitTemplate rabbitTemplate) {
+        return new RepublishMessageRecoverer(rabbitTemplate, "error.direct", serviceName);
     }
 }
